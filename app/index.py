b16c8fe5a2fa8e7df3474423bb858180c6129634
leviqhribeiro <leviqhrlevi@gmail.com>
2024-06-19 14:55:32 -0300

    Função para gerar a Coluna de Data de Execução função para iterar na coluna Data de Execução!

--- app/index.py
+++ app/index.py
@@ -1,30 +1,25 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 import pdfplumber
 import pandas as pd
 import os
 from io import BytesIO
 
-# Configuração da página do Streamlit
+# Configuração da página
 st.set_page_config(layout="wide", page_title="Auditar Engenharia")
 
 # Interface do usuário na barra lateral
 with st.sidebar:
     st.header("CONVERSOR DE ATIVIDADES")
-    arquivo_pedido = st.file_uploader(
-        label="Selecione o Arquivo PDF ou EXCEL das atividades:", 
-        type=['pdf', 'xlsx']
+    arquivo_atividades = st.file_uploader(
+        label= "Selecione o arquivo PDF ou EXCEL das Atividades:",
+        type= ("pdf", "xlsx")
     )
 
-url = ""
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-# Definindo possíveis nomes de colunas
+# Definindo possíveis nomes das colunas
 possiveis_nomes_colunas = {
-    "Atividade": ["Nome da Tarefa", "Nome da Atividade", "Atividade", "Tarefa"],
-    "Data Inicio": ["Início", "Data de Inicio", "Data Inicial", "Data a ser Iniciada"],
-    "Data Termino": ["Término", "Data de Termino", "Data Final", "Data a ser Concluida", "Data Conclusao"]
+    "Atividade": ["ITEM", "Nome da Tarefa", "Nome da Atividade", "Atividade", "Tarefa"],
+    "Data Inicio": ["Início", "INÍCIO", "Data de Inicio", "Data Inicial", "Data a ser Iniciada"],
+    "Data Termino": ["Término", "TÉRMINO", "Data de Termino", "Data Final", "Data a ser Concluida", "Data Conclusao"]
 }
 
 # Função para renomear colunas do DataFrame
@@ -57,18 +52,35 @@ def extrair_datas(caminho_pdf):
 
 # Função para calcular os dias de atividade e repetir o nome da atividade
 def calcular_dias_atividade(df):
-    df['Data Inicio'] = pd.to_datetime(df['Data Inicio'], format='%d/%m/%Y')
-    df['Data Termino'] = pd.to_datetime(df['Data Termino'], format='%d/%m/%Y')
+    df['Data Inicio'] = pd.to_datetime(df['Data Inicio'], errors='coerce')
+    df['Data Termino'] = pd.to_datetime(df['Data Termino'], errors='coerce')
     df['Dias de Atividade'] = (df['Data Termino'] - df['Data Inicio']).dt.days + 1
-    df_repetida = pd.DataFrame()
+    
+    df_final = pd.DataFrame()
     for _, row in df.iterrows():
         df_temp = pd.DataFrame({
             'Atividade': [row['Atividade']] * row['Dias de Atividade'],
             'Data Inicio': [row['Data Inicio']] * row['Dias de Atividade'],
-            'Data Termino': [row['Data Termino']] * row['Dias de Atividade']
+            'Data Termino': [row['Data Termino']] * row['Dias de Atividade'],
+            'Data de Execução': [row['Data Inicio'] + pd.Timedelta(days=i) for i in range(row['Dias de Atividade'])],
         })
-        df_repetida = pd.concat([df_repetida, df_temp], ignore_index=True)
-    return df_repetida
+        df_final = pd.concat([df_final, df_temp], ignore_index=True)
+
+    # Formatando as colunas de data para 'dd/mm/yyyy' e removendo as horas
+    df_final['Data Inicio'] = df_final['Data Inicio'].dt.strftime('%d/%m/%Y')
+    df_final['Data Termino'] = df_final['Data Termino'].dt.strftime('%d/%m/%Y')
+    df_final['Data de Execução'] = df_final['Data de Execução'].dt.strftime('%d/%m/%Y')
+    
+    #Função para iterar em cada linha da coluna Data de Execução
+    for indice, linha in df_final.iterrows():
+        data_de_execucao = linha['Data de Execução']
+        # Faça o que deseja com o valor da atividade
+        st.write(f"Data de Execução: {data_de_execucao}")
+
+    return df_final
+
+#para cada dia da data de execução:
+#    criar nova planilha referente aos dias da data de execução        
 
 # Função para converter DataFrame em um arquivo Excel 
 def to_excel(df):
@@ -80,11 +92,11 @@ def to_excel(df):
     return processed_data
 
 # Verificação e processamento do arquivo carregado
-if arquivo_pedido is not None:
-    if arquivo_pedido.type == "application/pdf":
+if arquivo_atividades is not None:
+    if arquivo_atividades.type == "application/pdf":
         with st.spinner('Processando arquivo PDF...'):
             with open("temp.pdf", "wb") as f:
-                f.write(arquivo_pedido.read())
+                f.write(arquivo_atividades.read())
 
             atividades, data_inicio, data_termino = extrair_datas("temp.pdf")
             os.remove("temp.pdf")
@@ -98,6 +110,7 @@ if arquivo_pedido is not None:
                 dias_de_atividade = calcular_dias_atividade(dias_de_atividade)
                 st.dataframe(dias_de_atividade)
 
+                
                 # Adicionar botão de download
                 excel_data = to_excel(dias_de_atividade)
                 st.download_button(
@@ -107,11 +120,11 @@ if arquivo_pedido is not None:
                     mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
                 )
             else:
-                st.error("Atividades, datas de início e/ou término não encontradas no arquivo PDF.")
+                st.error("Atividade, Datas de início e/ou término não encontradas no arquivo PDF.")
 
-    elif arquivo_pedido.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+    elif arquivo_atividades.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         with st.spinner('Processando arquivo Excel...'):
-            df = pd.read_excel(arquivo_pedido)
+            df = pd.read_excel(arquivo_atividades)
             df = renomear_colunas(df, possiveis_nomes_colunas)
             if 'Atividade' in df.columns and 'Data Inicio' in df.columns and 'Data Termino' in df.columns:
                 df = calcular_dias_atividade(df)
